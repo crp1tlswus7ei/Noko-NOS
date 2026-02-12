333d54ec4e5b1db2e9d22e703abddffb9819eab6
suomi <crp1tlswus7ei@gmail.com>
2026-02-11 22:37:33 -0600

    New Class and Changes New "Advice" view, two buttons for accept and denied, subtle changes in Delete view

--- misc/Buttons.py
+++ misc/Buttons.py
@@ -2,9 +2,45 @@ import discord
 from discord.ui import View, Button
 from misc.Exceptions import *
 
+# advice
+class Advice(discord.ui.View):
+   def __init__(
+           self,
+           interaction: discord.Interaction
+   ):
+      super().__init__(timeout = 49)
+      self.interaction = interaction
+      self.confirmed = False
+
+   @discord.ui.button(
+      emoji = '<:white_check:1470874033863262220>',
+      style = discord.ButtonStyle.green,
+   )
+   async def accept_(
+           self,
+           interaction: discord.Interaction,
+           button: discord.ui.Button
+   ):
+      self.confirmed = True
+      await interaction.response.defer()
+      self.stop()
+
+   @discord.ui.button(
+      emoji = '<:white_cross:1405656979266867210>',
+      style = discord.ButtonStyle.red
+   )
+   async def denied_(
+           self,
+           interaction: discord.Interaction,
+           button: discord.ui.Button
+   ):
+      await interaction.response.defer()
+      self.stop()
+
+# delete/dimiss
 class Delete(View):
    def __init__(self):
-      super().__init__(timeout = None)
+      super().__init__(timeout = 49)
 
       delete_button = Button(
          emoji = '<:white_cross:1405656979266867210>',
@@ -24,8 +60,11 @@ class Delete(View):
          )
          return
 
+      # primary
+      await interaction.response.defer()
       await interaction.message.delete()
 
+# Docs
 class Forbidden(discord.ui.View):
    def __init__(self):
       super().__init__()
